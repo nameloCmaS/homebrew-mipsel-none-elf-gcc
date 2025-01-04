4801c774bb6273fc09c5ca82b19568c074811afa
nameloCmaS <namelocmas@gmail.com>
2025-01-04 22:08:14 +0000

    Addressing errors from `brew test-bot --only-tap-syntax`

--- Formula/mipsel-none-elf-gcc.rb
+++ Formula/mipsel-none-elf-gcc.rb
@@ -1,13 +1,21 @@
 class MipselNoneElfGcc < Formula
-  desc "The GNU compiler collection for mipsel"
+  desc "GNU compiler collection for mipsel-none-elf"
   homepage "https://gcc.gnu.org"
   url "https://ftp.gnu.org/gnu/gcc/gcc-14.2.0/gcc-14.2.0.tar.gz"
   sha256 "7d376d445f93126dc545e2c0086d0f647c3094aae081cdb78f42ce2bc25e7293"
 
   depends_on "gmp"
-  depends_on "mipsel-none-elf-binutils"
   depends_on "libmpc"
   depends_on "mpfr"
+  depends_on "mipsel-none-elf-binutils"
+
+  # Branch from the Darwin maintainer of GCC, with a few generic fixes and
+  # Apple Silicon support, located at https://github.com/iains/gcc-14-branch
+  # Taken from the `gcc` Formula code: https://formulae.brew.sh/formula/gcc
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/f30c309442a60cfb926e780eae5d70571f8ab2cb/gcc/gcc-14.2.0-r2.diff"
+    sha256 "6c0a4708f35ccf2275e6401197a491e3ad77f9f0f9ef5761860768fa6da14d3d"
+  end
 
   def install
     mkdir "mipsel-none-elf-gcc-build" do
@@ -30,14 +38,6 @@ class MipselNoneElfGcc < Formula
       system "make", "all-target-libgcc"
       system "make", "install-target-libgcc"
     end
-  end
-  
-  # Branch from the Darwin maintainer of GCC, with a few generic fixes and
-  # Apple Silicon support, located at https://github.com/iains/gcc-14-branch
-  # Taken from the `gcc` Formula code: https://formulae.brew.sh/formula/gcc
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/f30c309442a60cfb926e780eae5d70571f8ab2cb/gcc/gcc-14.2.0-r2.diff"
-    sha256 "6c0a4708f35ccf2275e6401197a491e3ad77f9f0f9ef5761860768fa6da14d3d"
   end
 
   test do
