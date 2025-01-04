0bc56333b22f9088c2b226048b2f917131e92e52
nameloCmaS <namelocmas@gmail.com>
2025-01-04 21:28:03 +0000

    Added 2 x Formula from gist

---
+++ Formula/mipsel-none-elf-binutils.rb
@@ -0,0 +1,19 @@
+class MipselNoneElfBinutils < Formula
+  desc "FSF Binutils for mipsel cross development"
+  homepage "https://www.gnu.org/software/binutils/"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.43.tar.gz"
+  sha256 "025c436d15049076ebe511d29651cc4785ee502965a8839936a65518582bdd64"
+
+  def install
+    system "./configure", "--target=mipsel-none-elf",
+                          "--disable-multilib",
+                          "--disable-nls",
+                          "--disable-werror",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+  test do
+    assert_match "f()", shell_output("#{bin}/mipsel-none-elf-c++filt _Z1fv")
+  end
+end
---
+++ Formula/mipsel-none-elf-gcc.rb
@@ -0,0 +1,54 @@
+class MipselNoneElfGcc < Formula
+  desc "The GNU compiler collection for mipsel"
+  homepage "https://gcc.gnu.org"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-14.2.0/gcc-14.2.0.tar.gz"
+  sha256 "7d376d445f93126dc545e2c0086d0f647c3094aae081cdb78f42ce2bc25e7293"
+
+  depends_on "gmp"
+  depends_on "mipsel-none-elf-binutils"
+  depends_on "libmpc"
+  depends_on "mpfr"
+
+  def install
+    mkdir "mipsel-none-elf-gcc-build" do
+      system "../configure", "--target=mipsel-none-elf",
+                             "--prefix=#{prefix}",
+                             "--without-isl",
+                             "--disable-nls",
+                             "--disable-threads",
+                             "--disable-shared",
+                             "--disable-libssp",
+                             "--disable-libstdcxx-pch",
+                             "--disable-libgomp",
+                             "--disable-werror",
+                             "--without-headers",
+                             "--with-as=#{Formula["mipsel-none-elf-binutils"].bin}/mipsel-none-elf-as",
+                             "--with-ld=#{Formula["mipsel-none-elf-binutils"].bin}/mipsel-none-elf-ld",
+                             "--enable-languages=c,c++"
+      system "make", "all-gcc"
+      system "make", "install-gcc"
+      system "make", "all-target-libgcc"
+      system "make", "install-target-libgcc"
+    end
+  end
+  
+  # Branch from the Darwin maintainer of GCC, with a few generic fixes and
+  # Apple Silicon support, located at https://github.com/iains/gcc-14-branch
+  # Taken from the `gcc` Formula code: https://formulae.brew.sh/formula/gcc
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/f30c309442a60cfb926e780eae5d70571f8ab2cb/gcc/gcc-14.2.0-r2.diff"
+    sha256 "6c0a4708f35ccf2275e6401197a491e3ad77f9f0f9ef5761860768fa6da14d3d"
+  end
+
+  test do
+    (testpath/"test-c.c").write <<~EOS
+      int main(void)
+      {
+        int i=0;
+        while(i<10) i++;
+        return i;
+      }
+    EOS
+    system "#{bin}/mipsel-none-elf-gcc", "-c", "-o", "test-c.o", "test-c.c"
+  end
+end
